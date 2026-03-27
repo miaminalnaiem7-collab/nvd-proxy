71cf579aa58cd8357096bf9a0a557d5d627696c4
miaminalnaiem7-collab <miaminalnaiem7@gmail.com>
2026-03-27 18:03:50 +0400

    Update cve.js

--- api/cve.js
+++ api/cve.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        "query": keyword
+        "search": keyword
       })
     });
 
